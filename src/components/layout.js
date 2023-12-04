212f5899db53ea32e7b3fdb663140eb9193bccf9
Kusal Dissanayake <kusaldissanayake2@gmail.com>
2023-12-04 21:17:09 +0530

    Finalized

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Output from './output';
 
 const Layout = (props) => {
-    let[input, setInput] = useState('0');
-    let[result,setResult] = useState('')
+    let [input, setInput] = useState('0');
+    let [result,setResult] = useState('')
 
     const handleClick = (event) => {
         const value = event.target.value;
@@ -24,14 +24,14 @@ const Layout = (props) => {
                 }                   
                 }
             }else if( value === 'C'){
-                setResult('0')
-                setInput('')
+                setResult('')
+                setInput('0')
             }
             else if(value === 'DEL'){
                 let str = input;
                 str = str.substr(0, str.length-1)
                 setInput(str);
-            }else if(input = '0'){
+            }else if(input === '0'){
                 setInput(value);
             }else{
                 setInput((input += value))
@@ -56,7 +56,7 @@ const Layout = (props) => {
                     <div className='col'><input type="button" value={"7"} className='btn btn-light button-num' onClick={handleClick}/></div>
                     <div className='col'><input type="button" value={"8"} className='btn btn-light button-num' onClick={handleClick}/></div>
                     <div className='col'><input type="button" value={"9"} className='btn btn-light button-num' onClick={handleClick}/></div>
-                    <div className='col'><input type="button" value={"x"} className='btn btn-primary button-op' onClick={handleClick}/></div>
+                    <div className='col'><input type="button" value={"*"} className='btn btn-primary button-op' onClick={handleClick}/></div>
                     </div>
                     <div className="row g-0">
                     <div className='col'><input type="button" value={"4"} className='btn btn-light button-num' onClick={handleClick}/></div>
--- src/components/output.js
+++ src/components/output.js
@@ -4,7 +4,7 @@ import OutputRow from './outputRow';
 const Output = props => {
     return(
         <>
-        <OutputRow value={props.answer} textSize={{ fontSize: '20px' }} />
+        <OutputRow value={props.answer} textSize={{ fontSize: '15px' }} />
         <OutputRow value={props.user}/>
         </>
     );
